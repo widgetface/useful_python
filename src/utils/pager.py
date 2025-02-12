0ca2a73c3d46cd68889d356ad359bba512ef6ebd
Groggie <groggy67@yahoo.com>
2025-02-12 14:56:32 +0000

    Fix type hinting

--- src/utils/pager.py
+++ src/utils/pager.py
@@ -1,29 +1,25 @@
-from typing import Iterator, Union, List
+from typing import Iterator, Iterable, List, Tuple, Union
 from itertools import batched
 
 # Using batched to paginate
-Element = Union[str, int]
 
 
 class Pager:
-    def __init__(self, pages: List[Element], page_size: int = 10):
+    def __init__(self, pages: Iterable, page_size: int = 10):
         self.pages: Iterator = batched(pages, page_size)
         # Initialise empty navigation caches.
-        self.prev_pages: List[Element] = []
-        self.next_pages: List[Element] = []
+        self.prev_pages: List[Tuple] = []
+        self.next_pages: List[Tuple] = []
 
-    def next_page(self):
+    def next_page(self) -> Tuple:
         """Gets the next page of results or None."""
         # Get the next page, possibly from the navigation cache.
-        next_page: Element = (
-            self.next_pages.pop() if self.next_pages else next(self.pages, None)
-        )
+        next_page = self.next_pages.pop() if self.next_pages else next(self.pages, None)
         if next_page is not None:
             self.prev_pages.append(next_page)
-
         return next_page
 
-    def prev_page(self):
+    def prev_page(self) -> Tuple:
         """Gets the previous page of results or None."""
         # The last page in prev_pages was the last one sent, so we need
         # to move it to `next_pages`.
